9f1c6e598184bb59e1df782ab2961cff7a2d6cf5
Alex Tatulchenkov <webtota@gmail.com>
2017-12-09 14:07:46 +0300

    increase gas

--- truffle.js
+++ truffle.js
@@ -8,7 +8,8 @@ module.exports = {
             provider: function() {
                 return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/2t9moYtXJAAKeyV0BTng")
             },
-            network_id: 3
+            network_id: 3,
+            gas: 1712388
         }
     }
 };
